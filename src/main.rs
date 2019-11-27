ffca3df8fd59f46f7d261dfe77c47558d7bfd076
Marco Napetti <marco.napetti@athonet.com>
2019-11-27 21:43:44 +0100

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ async fn main() -> Result<(), ()> {
     let conn = Conn::new(&env::var("DATABASE_URL").map_err(|_| error!("Missing env var DATABASE_URL"))?)
         .await.map_err(|e| error!("Can't connect to MySQL: {}", e))?;
 
-    let res = conn.query("SELECT value FROM metadata WHERE `key` = 'DEVICEAPI_HOST_WHITELIST'").await.map_err(|e| error!("Can't query MySQL: {}", e))?;
+    let res = conn.query("SELECT value FROM metadata WHERE `key` = 'DEVICEAPI_HOST_WHITELIST'")
+        .await.map_err(|e| error!("Can't query MySQL: {}", e))?;
 
     let (_, ips) = res.reduce_and_drop(Vec::new(), |mut v, mut row| {
             let value: String = row.take("value").expect("MySQL metadata.value decode error");
@@ -30,7 +31,7 @@ async fn main() -> Result<(), ()> {
     for address in response.iter() {
         let s = format!("{}", address);
         if !ips.contains(&s) {
-            println!("DEVICEAPI_HOST_WHITELIST doesn't containts {}", s);
+            println!("DEVICEAPI_HOST_WHITELIST doesn't contains {}", s);
         }
     }
 
